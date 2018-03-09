90d6870d5ce34f74dc87bca5e94c873bf21d33d3
Breathetaker613 <p.spyropoylos.doc@gmail.com>
2018-03-09 17:29:52 +0200

    login activity and xml

--- app/src/main/java/com/example/intered/interedproject/Main2Activity.java
+++ app/src/main/java/com/example/intered/interedproject/Main2Activity.java
@@ -1,7 +1,10 @@
 package com.example.intered.interedproject;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -10,10 +13,32 @@ public class Main2Activity extends AppCompatActivity {
     Button loginbtn;
     EditText emailText;
     EditText nameText;
+    String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        nameText=(EditText)findViewById(R.id.nametxt);
+        emailText=(EditText)findViewById(R.id.emailtxt);
+
+        loginbtn=(Button)findViewById(R.id.button);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                name = nameText.getText().toString();
+
+                SharedPreferences.Editor editor = getPreferences(MODE_PRIVATE).edit();
+                editor.putString("text",name);
+                editor.apply();
+
+            }
+        });
+
+
+
     }
 }
