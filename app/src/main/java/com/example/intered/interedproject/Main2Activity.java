e96316e90646b6648d7081611f68be2ac91c8e34
Breathetaker613 <p.spyropoylos.doc@gmail.com>
2018-03-09 16:52:01 +0200

    login activity and xml

--- app/src/main/java/com/example/intered/interedproject/Main2Activity.java
+++ app/src/main/java/com/example/intered/interedproject/Main2Activity.java
@@ -2,8 +2,14 @@ package com.example.intered.interedproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Main2Activity extends AppCompatActivity {
+
+    Button loginbtn;
+    EditText emailText;
+    EditText nameText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
