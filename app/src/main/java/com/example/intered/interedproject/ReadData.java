eecd04eb66fbe1d573eb22de25eebde58f3b82ac
Breathetaker613 <p.spyropoylos.doc@gmail.com>
2018-03-30 17:20:40 +0300

    Read data activity

--- app/src/main/java/com/example/intered/interedproject/ReadData.java
+++ app/src/main/java/com/example/intered/interedproject/ReadData.java
@@ -2,12 +2,19 @@ package com.example.intered.interedproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReadData extends AppCompatActivity {
+
+    TextView textView1;
+    TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read_data);
+
+        textView1=(TextView)findViewById(R.id.text);
+        textView2=(TextView)findViewById(R.id.name);
     }
 }
