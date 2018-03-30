50c6ca95c94b7b043ab27cfe0c3535dd30ad7c15
StergiosMamoulakis <mamoulakis@live.com>
2018-03-30 16:28:28 +0300

    test2

--- app/src/main/java/com/example/intered/interedproject/MainActivity.java
+++ app/src/main/java/com/example/intered/interedproject/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.intered.interedproject;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -21,10 +23,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtName= (TextView)findViewById(R.id.Name);
-        btnPost= (Button)findViewById(R.id.B_post);
-        editTextPost= (EditText)findViewById(R.id.editTextPost);
-        txtEmail= (TextView)findViewById(R.id.emailtxt);
+        txtName = (TextView) findViewById(R.id.Name);
+        btnPost = (Button) findViewById(R.id.B_post);
+        editTextPost = (EditText) findViewById(R.id.editTextPost);
+        txtEmail = (TextView) findViewById(R.id.emailtxt);
 
 
         SharedPreferences prefs = getSharedPreferences("save", MODE_PRIVATE);
@@ -34,5 +36,14 @@ public class MainActivity extends AppCompatActivity {
 
         txtName.setText(name);
         txtEmail.setText(email);
+    }
+
+
+    public void onBackPressed() {
+        Log.d("CDA", "onBackPressed Called");
+        Intent setIntent = new Intent(Intent.ACTION_MAIN);
+        setIntent.addCategory(Intent.CATEGORY_HOME);
+        setIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(setIntent);
     }
 }
